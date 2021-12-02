ab11184be99c137cf723fdd2774f6bea962cd462
Janne Johansson <23361445+JJJohansson@users.noreply.github.com>
2021-12-02 10:47:29 +0200

    Day 2

---
+++ 02/advent_02122021.js
@@ -0,0 +1,35 @@
+// PASTE THESE IN WEB BROWSER CONSOLE AT https://adventofcode.com/2021/day/2/input
+
+// FIRST TASK
+(() => {
+	const moves = document.querySelector("pre").textContent.toString().split('\n');
+	let horizontalPos = 0;
+	let depth = 0;
+	for (let move of moves) {
+		const direction = move.split(' ')[0];
+		const value = parseInt(move.split(' ')[1]);
+		if (direction.match('forward')) horizontalPos += value;
+		if (direction.match('up')) depth -= value;
+		if (direction.match('down')) depth += value;
+	}
+	console.log('Answer for the first one is', horizontalPos * depth);
+})();
+
+// SECOND TASK
+(() => {
+	const moves = document.querySelector("pre").textContent.toString().split('\n');
+	let horizontalPos = 0;
+	let depth = 0;
+	let aim = 0;
+	for (let move of moves) {
+		const direction = move.split(' ')[0];
+		const value = parseInt(move.split(' ')[1]);
+		if (direction.match('forward')) {
+			horizontalPos += value;
+			depth += (aim * value);
+		}
+		if (direction.match('up')) aim -= value;
+		if (direction.match('down')) aim += value;
+	}
+	console.log('Answer for the first one is', horizontalPos * depth);
+})();
