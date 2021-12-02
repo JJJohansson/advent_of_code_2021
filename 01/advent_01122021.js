a3374d451bbb2eba06c680f8a625c78af8e21d43
Janne Johansson <23361445+JJJohansson@users.noreply.github.com>
2021-12-02 13:42:38 +0200

    small formatting

--- 01/advent_01122021.js
+++ 01/advent_01122021.js
@@ -5,22 +5,22 @@
 	const measurements = document.querySelector("pre").textContent.split('\n').map(Number);
 	let answer = 0;
 	for (let i = 0; i < measurements.length; i++) {
-		if (measurements[i] > measurements[i-1]) {
-			answer++;
-		}
+		if (measurements[i] > measurements[i-1]) answer++;
 	}
-	console.log('Answer for the first one is', answer);
+	console.log('Answer for the first task is', answer);
 })();
 
 // SECOND TASK
 (() => {
 	const measurements = document.querySelector("pre").textContent.split('\n').map(Number);
 	let answer = 0;
-	let oldWindowSum = 0;
-	for (let i = 1; i < measurements.length-2; i++) {
-		let windowSum = measurements[i-1] + measurements[i] + measurements[i+1];
-		if (oldWindowSum != 0 && oldWindowSum < windowSum) answer++;
-   		oldWindowSum = windowSum;
+	let windowSum = 0;
+	for (let i = 0; i < measurements.length; i++) {
+		let window = [measurements[i-1], measurements[i], measurements[i+1]];
+		if (window.includes(undefined)) continue;
+		let newWindowSum = window.reduce((total, num) => total + num);
+		if (windowSum != 0 && windowSum < newWindowSum) answer++;
+   		windowSum = newWindowSum;
 	}
-	console.log('Answer for the second one is', answer);
+	console.log('Answer for the second task is', answer);
 })();
--- 02/advent_02122021.js
+++ 02/advent_02122021.js
@@ -5,31 +5,31 @@
 	const moves = document.querySelector("pre").textContent.split('\n');
 	let horizontalPos = 0;
 	let depth = 0;
-	for (let move of moves) {
+	for (const move of moves) {
 		const direction = move.split(' ')[0];
 		const value = parseInt(move.split(' ')[1]);
 		if (direction.match('forward')) horizontalPos += value;
 		if (direction.match('up')) depth -= value;
 		if (direction.match('down')) depth += value;
 	}
-	console.log('Answer for the first one is', horizontalPos * depth);
+	console.log('Answer for the first task is', horizontalPos * depth);
 })();
 
 // SECOND TASK
 (() => {
-	const moves = document.querySelector("pre").textContent.toString().split('\n');
+	const moves = document.querySelector("pre").textContent.split('\n');
 	let horizontalPos = 0;
 	let depth = 0;
 	let aim = 0;
 	for (const move of moves) {
 		const direction = move.split(' ')[0];
-		const value = parseInt(move.split(' ')[1]);
+		const amount = parseInt(move.split(' ')[1]);
 		if (direction.match('forward')) {
-			horizontalPos += value;
-			depth += (aim * value);
+			horizontalPos += amount;
+			depth += (aim * amount);
 		}
-		if (direction.match('up')) aim -= value;
-		if (direction.match('down')) aim += value;
+		if (direction.match('up')) aim -= amount;
+		if (direction.match('down')) aim += amount;
 	}
-	console.log('Answer for the second one is', horizontalPos * depth);
+	console.log('Answer for the second task is', horizontalPos * depth);
 })();
